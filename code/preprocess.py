b044f799c13b89683f215bf58d65f7ef873f179f
lunes22 <145524022+lunes22@users.noreply.github.com>
2026-02-15 02:39:21 +0900

    260215_주석수정

--- code/preprocess.py
+++ code/preprocess.py
@@ -5,7 +5,7 @@ import torch
 from torchvision import transforms as T
 from torchvision.utils import save_image  # 저장을 위한 라이브러리 추가
 
-# 1. 설정 및 경로
+# 설정 및 경로
 INPUT_DIR = './data_sbP'
 OUTPUT_DIR = './processed_data'
 IMG_SIZE = 448 
@@ -15,7 +15,7 @@ if os.path.exists(OUTPUT_DIR):
     shutil.rmtree(OUTPUT_DIR)
 os.makedirs(OUTPUT_DIR)
 
-# 2. 전처리 파이프라인 (팀장님 가이드)
+# 전처리 파이프라인
 def get_resnet_preprocess(size=224):
     return T.Compose([
         T.Resize((size, size)),
@@ -44,8 +44,8 @@ def process_and_save():
             processed_data = preprocess(image)
 
             # [핵심] 실제 파일로 저장하는 로직
-            # Normalize된 이미지는 수치가 변해있으므로 시각화용으로 저장할 때는 주의가 필요하지만, 
-            # 일단 파일 생성을 확인하기 위해 저장합니다.
+            # Normalize된 이미지는 수치가 변해있으므로 시각화용으로 저장할 때는 주의가 필요
+            # 파일 생성 및 저장 확인을 위해 우선 저장이 되도록 작성
             output_path = os.path.join(label_dir, file_name)
             save_image(torch.as_tensor(processed_data), os.path.join(label_dir, file_name))
             if (i + 1) % 50 == 0:
